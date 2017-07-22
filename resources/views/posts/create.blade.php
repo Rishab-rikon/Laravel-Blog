2000ca24219ca707864226ab29b2c4aa89a949d4
Rishab-rikon <rishabrocking20@gmail.com>
2017-07-22 14:53:53 +0530

    Initial commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,21 @@
+@extends('main')
+
+@section('title', '| Create new posts')
+
+@section('content')
+	<div class="row">
+		<div class="col-md-8 col-offset-2">
+			<h1>Create New Post</h1>
+			<hr>
+			{!! Form::open(['route' => 'posts.store']) !!}
+    			{{Form::label('title', "Title:")}}
+    			{{Form::text('title', null, array('class' => 'form-control'))}}
+				<hr>
+    			{{Form::label('body', "Post Body:")}}
+    			{{Form::textarea('body', null, array('class' => 'form-control'))}}
+				<hr>
+    			{{Form::submit('Create Post', array('class' => 'btn btn-success btn-lg'))}}
+			{!! Form::close() !!}
+		</div>
+	</div>
+@endsection
